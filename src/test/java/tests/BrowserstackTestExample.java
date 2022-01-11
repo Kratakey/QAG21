8fcd99c32e7267ebb347a18302b6b11153b0ed41
Kratakey <Kratakey@gmail.com>
2022-01-11 10:32:23 +0300

    fix

--- src/test/java/tests/BrowserstackTestExample.java
+++ src/test/java/tests/BrowserstackTestExample.java
@@ -3,25 +3,24 @@ package tests;
 import com.codeborne.selenide.Condition;
 import config.TestBase;
 import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.*;
 
+@Tag("selenide_android")
 public class BrowserstackTestExample extends TestBase {
 
     @Test
-    @Tag("selenide_android")
     void exampleTest() {
         $(MobileBy.AccessibilityId("Search Wikipedia")).click();
         $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
-        sleep(5000);
         $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container")).shouldHave(sizeGreaterThan(0));
     }
 
     @Test
-    @Tag("selenide_android")
     void secondTest() {
         $(MobileBy.id("org.wikipedia.alpha:id/menu_overflow_button")).click();
         $(MobileBy.id("org.wikipedia.alpha:id/explore_overflow_settings")).click();
